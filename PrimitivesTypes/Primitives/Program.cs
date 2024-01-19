e5f3fb09c9842cdfa79274e6b1b78ce535ffa512
Bruno Beluco <beluco.bispo@hotmail.com>
2024-01-19 16:37:19 -0300

    Primitive Types

---
+++ PrimitivesTypes/Primitives/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PrimitivesTypes {
+    internal class Primitives {
+        static void Main(string[] args) {
+            SByte x = 100;
+            int y = 10000;
+            float z = 5.4f;
+            double b = 6.6;
+            long a = 12L;
+            char c = 'a';
+            char euro = '\u00A3';
+            string name = "Bruno"; //This isnt a primitive type but it is a very common type
+
+
+            System.Console.WriteLine(x);
+            System.Console.WriteLine(y);
+            System.Console.WriteLine(z);
+            System.Console.WriteLine(a);
+            System.Console.WriteLine(b);
+            System.Console.WriteLine(c);
+            System.Console.WriteLine(euro);
+            System.Console.WriteLine(name);
+        }
+    }
+}
